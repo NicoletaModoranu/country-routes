3fe0e3fa28c08d11d7bdad77437c30060c9fbf21
nicoleta <nicoleta_corocea@yahoo.com>
2023-01-25 19:38:09 +0200

    formatted path finder service

--- src/main/java/com/country/routes/service/PathFinderService.java
+++ src/main/java/com/country/routes/service/PathFinderService.java
@@ -12,26 +12,24 @@ import java.util.stream.Collectors;
 @Service
 public class PathFinderService {
 
-    // BFS traversal that updates the dist and parent vector
-
     public PathResponse findRoute(String origin, String destination, List<Country> countriesList) {
 
         Map<String, CountryNode> allCountries = countriesList.stream().collect(Collectors.toMap(Country::getIdentifier, country -> new CountryNode(country.getIdentifier(), country.getBorders())));
 
-        Queue<CountryNode> queue = new LinkedList<>();// queue to store nodes to be visited along the breadth
+        Queue<CountryNode> queue = new LinkedList<>();
 
         CountryNode start = allCountries.get(origin);
         CountryNode end = allCountries.get(destination);
-        start.setVisited(true); // mark source node as visitedi
-        queue.add(start); // push src node to queue
+        start.setVisited(true);
+        queue.add(start);
 
         while (!queue.isEmpty()) {
-            CountryNode countryNode = queue.poll();// traverse all nodes along the breadth
-            // traverse along the node's breadth
+            CountryNode countryNode = queue.poll();
+
             for (String nodeName : countryNode.getBorders()) {
                 CountryNode node = allCountries.get(nodeName);
                 if (node != null && Boolean.FALSE.equals(node.isVisited())) {
-                    node.setVisited(true);// // mark it visited
+                    node.setVisited(true);
                     queue.add(node);
                     node.setPrev(countryNode);
                     if (node == end) {
@@ -47,10 +45,8 @@ public class PathFinderService {
 
     private PathResponse traceRoute(String destination, Map<String, CountryNode> allCountries) {
         CountryNode node = allCountries.get(destination);
-
         List<String> routeList = new ArrayList<>();
-        //Loop until node is null to reach start node
-        //becasue start.prev == null
+
         while (node != null) {
             routeList.add(node.getIdentifier());
             node = node.getPrev();
@@ -59,6 +55,7 @@ public class PathFinderService {
             throw new RouteException("No route found");
         }
         Collections.reverse(routeList);
+
         return new PathResponse(routeList);
     }
 }
